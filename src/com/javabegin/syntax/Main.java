b1bb32add5e596f05d6feb530901e85ce2389300
IvanPavlovets <ivan150287@gmail.com>
2016-07-11 12:00:04 +0700

    create obj car & invocation showInfo

--- src/com/javabegin/syntax/Main.java
+++ src/com/javabegin/syntax/Main.java
@@ -3,5 +3,8 @@ package com.javabegin.syntax;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git");
+
+        Car car1 = new Car(1, "Subaru");
+        car1.showInfo();
     }
 }
