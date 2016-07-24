df8e34233e60a9c08273d3df020919cc2aec8533
IvanPavlovets <ivan150287@gmail.com>
2016-07-24 14:16:30 +0700

    add test4() in class  Main and invoke it

--- src/com/javabegin/syntax/Main.java
+++ src/com/javabegin/syntax/Main.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git");
 
-        Car car1 = new Car(1, "Subaru");
+        Car car1 = new Car(2, "Subaru");
+        Car car2 = new Car(3, "Nissan");
         Main main = new Main();
 
         main.test1(car1);
         main.test2(car1.getI());
+        main.test4(car1,car2);
         car1.showInfo();
+        car2.showInfo();
 
 
     }
@@ -21,5 +24,9 @@ public class Main {
     public void test2(int i){
         i = 6;
         System.out.println("test2.i = "+i);
+    }
+
+    public void test4(Car... cars){
+        System.out.println("test4.cars.length = " + cars.length);
     }
 }
